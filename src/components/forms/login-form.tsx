7ad658b4ddb104ef71d3407bd42d7a846c1ab391
Julio <juliolk.4321@gmail.com>
2023-08-08 16:40:03 -0300

    validating and handling errors in form fields

--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -1,14 +1,35 @@
+import { useState } from 'react';
 import { Button } from '../buttons/button';
 import { FormField } from './form-field';
 import { FormContainer } from './form-styles';
 
 export const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  const handleFormSubmit = () => {
+    setIsButtonClicked(true);
+  };
+
   return (
     <FormContainer>
       <h1> Bem vindo(a) a Taqtile </h1>
-      <FormField label="Email" />
-      <FormField label="Senha" />
-      <Button title="Entrar" />
+      <FormField
+        label="Email"
+        value={email}
+        email
+        onChange={(value) => setEmail(value)}
+        isButtonClicked={isButtonClicked}
+      />
+      <FormField
+        label="Senha"
+        password
+        value={password}
+        onChange={(value) => setPassword(value)}
+        isButtonClicked={isButtonClicked}
+      />
+      <Button title="Entrar" onClick={handleFormSubmit} />
     </FormContainer>
   );
 };
