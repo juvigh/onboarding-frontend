c516b978bfd21a097b186c80e076d75be647c760
Julio <juliolk.4321@gmail.com>
2023-08-10 13:42:31 -0300

    Add LoginPage route and component

---
+++ src/pages/login/login-page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from './login-styles';
+import { LoginForm } from '../../components/forms/login-form';
+
+export const LoginPage = () => {
+  return (
+    <Container>
+      <LoginForm />
+    </Container>
+  );
+};
---
+++ src/pages/login/login-styles.ts
@@ -0,0 +1,6 @@
+import { styled } from 'styled-components';
+
+const MARGIN_SIDE = 400;
+export const Container = styled.div`
+  margin: 0 ${MARGIN_SIDE}px;
+`;
