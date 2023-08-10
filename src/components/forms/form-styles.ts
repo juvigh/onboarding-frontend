fb655fb4bce82c9cbaa98539c1366a4a264d7e7e
Julio <juliolk.4321@gmail.com>
2023-08-10 12:23:18 -0300

    Update form-styles.ts

--- src/components/forms/form-styles.ts
+++ src/components/forms/form-styles.ts
@@ -5,11 +5,7 @@ const GAP = 20;
 export const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
-<<<<<<< HEAD
   gap: ${GAP}px;
-=======
-  gap: 20px;
->>>>>>> Update form-styles.ts
 
   span {
     color: red;
