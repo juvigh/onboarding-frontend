90ba34fdb259603dee5706982c2c76e63239c08e
Julio <juliolk.4321@gmail.com>
2023-08-11 16:52:45 -0300

    Update button.tsx

--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { ButtonContainer } from './button-styles';
-import { LoginUserData } from '../../api/mutation/login-user';
 
 interface ButtonProps {
   title: string;
-  onClick?: (e: React.FormEvent) => Promise<string | LoginUserData | null | undefined | void>;
+  onClick?: (e: React.FormEvent) => Promise<string | null | undefined> | void;
 }
 
 export const Button = ({ title, onClick }: ButtonProps) => {
