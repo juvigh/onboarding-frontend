8c69c8495ceb8eee4078a285e22a8f4dae0dcb33
Julio <juliolk.4321@gmail.com>
2023-08-11 16:52:34 -0300

    Remove async function

--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -3,8 +3,6 @@ import { Button } from '../buttons/button';
 import { FormField } from './form-field';
 import { FormContainer } from './form-styles';
 import { useLoginMutation } from '../../api/fetch-login-user';
-import { FetchResult } from '@apollo/client';
-import { LoginUserData } from '../../api/mutation/login-user';
 import { ErrorMessage } from './error-message';
 
 export const LoginForm = () => {
@@ -18,25 +16,23 @@ export const LoginForm = () => {
   const isPasswordEmpty = !password.trim() && isButtonClicked;
   const isAnyFieldEmpty = isEmailEmpty || isPasswordEmpty;
 
-  const handleFormSubmit = async (e: React.FormEvent) => {
+  const handleFormSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     setIsButtonClicked(true);
 
     if (isAnyFieldEmpty) {
       return;
-    } else {
-      try {
-        const result: FetchResult<LoginUserData> = await loginUser({
-          variables: { data: { email, password } },
-        });
-
-        if (result.data && result.data.login.token) {
-          localStorage.setItem('authToken', result.data.login.token);
-        }
-      } catch (err) {
-        console.error('Erro na mutação', err);
-      }
     }
+
+    loginUser({
+      variables: { data: { email, password } },
+      onCompleted: (data) => {
+        localStorage.setItem('authToken', data.login.token);
+      },
+      onError: (error) => {
+        console.error('Erro na mutação', error);
+      },
+    });
   };
 
   return (
