561ecb9a4c4c6663111cf4ce0d01ce2a35fd8e37
Julio <juliolk.4321@gmail.com>
2023-08-14 09:44:14 -0300

    Add token to HomePage component

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,10 +1,21 @@
+import { useNavigate } from 'react-router-dom';
 import { UserList } from '../../components/user-list/user-list';
 import { Container } from './home-styles';
+import { useEffect } from 'react';
 
 export const HomePage = () => {
+  const token = localStorage.getItem('authToken') ?? '';
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token || token === '') {
+      navigate('/login');
+    }
+  }, [token]);
+
   return (
     <Container>
-      <UserList />
+      <UserList token={token} />
     </Container>
   );
 };
