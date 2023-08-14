37ada27454870c67653e086e6ed68bd7d6180d18
Julio <juliolk.4321@gmail.com>
2023-08-14 18:47:21 -0300

    Update button component styles

--- src/components/buttons/button-styles.ts
+++ src/components/buttons/button-styles.ts
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
 import { BUTTON_BORDER_RADIUS, BUTTON_PADDING } from '../../utils/constants';
 
-export const ButtonContainer = styled.button`
+interface ButtonContainerProps {
+  expand?: boolean;
+  bgColor?: string;
+  textColor?: string;
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
   padding: ${BUTTON_PADDING};
-  width: 100%;
-  background-color: blueviolet;
+  width: ${({ expand }) => (expand ? '100%' : 'auto')};
+  background-color: ${({ bgColor, disabled }) => (disabled ? '#d3d3d3' : bgColor || 'blueviolet')};
+  color: ${({ textColor, disabled }) => (disabled ? '#f5f5f5' : textColor || 'white')};
   border: none;
-  color: white;
   border-radius: ${BUTTON_BORDER_RADIUS};
   text-align: center;
 `;
