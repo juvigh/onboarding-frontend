76d4768743d7b911c5b93d40116fbb56ff16c6e3
Julio <juliolk.4321@gmail.com>
2023-08-14 12:00:45 -0300

    Update fetch-users.ts

--- src/api/fetch-users.ts
+++ src/api/fetch-users.ts
@@ -1,8 +1,21 @@
 import { useQuery } from '@apollo/client';
 import { GET_USERS, GetUserData } from './queries/get-users';
+interface FetchUsersOptions {
+  token: string;
+  page: number;
+  limit?: number;
+}
 
-export const fetchUsers = (token: string) => {
+export const fetchUsers = ({ token, page, limit = 4 }: FetchUsersOptions) => {
+  const offset = (page - 1) * limit;
   const { data, loading, error } = useQuery<GetUserData>(GET_USERS, {
+    variables: {
+      data: {
+        offset,
+        limit,
+      },
+    },
+
     context: {
       headers: {
         Authorization: token,
