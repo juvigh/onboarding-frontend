6376f65efe7e03f8cf0b24f71ccf69401236da11
Julio <juliolk.4321@gmail.com>
2023-08-14 12:00:48 -0300

    Update get-users.ts

--- src/api/queries/get-users.ts
+++ src/api/queries/get-users.ts
@@ -1,12 +1,19 @@
 import { gql } from '@apollo/client';
 
 export const GET_USERS = gql`
-  query Users {
-    users {
+  query User($data: PageInput) {
+    users(data: $data) {
       nodes {
         id
         name
         email
+      }
+      count
+      pageInfo {
+        offset
+        limit
+        hasNextPage
+        hasPreviousPage
       }
     }
   }
@@ -15,5 +22,12 @@ export const GET_USERS = gql`
 export interface GetUserData {
   users: {
     nodes: [{ id: string; name: string; email: string }];
+    count: number;
+    pageInfo: {
+      offset: number;
+      limit: number;
+      hasNextPage: boolean;
+      hasPreviousPage: boolean;
+    };
   };
 }
