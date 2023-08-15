5e2f5a96d9514b59bcbb2ef45f43b2b8e7faacb9
Julio <juliolk.4321@gmail.com>
2023-08-15 13:47:08 -0300

    Add RegisterPage route and component

---
+++ src/pages/register/register-page.tsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from '../../components/forms/register-form';
+import { Container } from './register-styles';
+
+export const RegisterPage = () => {
+  return (
+    <Container>
+      <RegisterForm />
+    </Container>
+  );
+};
