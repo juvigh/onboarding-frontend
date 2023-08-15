22adb0745305f3ad6289c4f9e61905356f3cb9d4
Julio <juliolk.4321@gmail.com>
2023-08-15 13:48:16 -0300

    Add create user button and change pagination component style

--- src/components/pagination/pagination-styles.ts
+++ src/components/pagination/pagination-styles.ts
@@ -2,6 +2,12 @@ import styled from 'styled-components';
 
 export const PaginationContainer = styled.div`
   display: flex;
+  justify-content: center;
+`;
+
+export const MenuPagination = styled.div`
+  display: flex;
+  flex: 1;
   align-items: center;
   justify-content: center;
 `;
