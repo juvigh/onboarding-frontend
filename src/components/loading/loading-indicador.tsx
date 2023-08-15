6513475763fcffbaccee7599e86f78008cdd5bab
Julio <juliolk.4321@gmail.com>
2023-08-15 15:02:26 -0300

    Update loading-indicador.tsx

--- src/components/loading/loading-indicador.tsx
+++ src/components/loading/loading-indicador.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { LoadingContainer } from './loading-styles';
 
-export const LoadingIndicator = () => {
-  return (
-    <LoadingContainer>
-      <img src="https://media.tenor.com/YtAOA9y7VG0AAAAM/loading.gif" alt="Carregando..." />
-    </LoadingContainer>
-  );
-};
+export const LoadingIndicator = () => (
+  <LoadingContainer>
+    <img src="https://media.tenor.com/YtAOA9y7VG0AAAAM/loading.gif" alt="Carregando..." />
+  </LoadingContainer>
+);
