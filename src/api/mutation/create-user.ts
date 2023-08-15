74ed4d41204f382fbde52fd3f11786fab81f406c
Julio <juliolk.4321@gmail.com>
2023-08-15 18:18:20 -0300

    Create create-user.ts

---
+++ src/api/mutation/create-user.ts
@@ -0,0 +1,14 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+  mutation CreateUser($data: UserInput!) {
+    createUser(data: $data) {
+      id
+      name
+      email
+      role
+      phone
+      birthDate
+    }
+  }
+`;
