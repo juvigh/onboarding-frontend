bcd10499b7cf78d46ee778d290c5085bd9b808c9
Julio <juliolk.4321@gmail.com>
2023-08-15 13:45:43 -0300

    Add style props to the button component

--- src/components/buttons/button-styles.ts
+++ src/components/buttons/button-styles.ts
@@ -1,3 +1,4 @@
+import { ACTION_BUTTON_PADDING } from './../../utils/constants';
 import styled from 'styled-components';
 import { BUTTON_BORDER_RADIUS, BUTTON_PADDING } from '../../utils/constants';
 
@@ -5,14 +6,17 @@ interface ButtonContainerProps {
   expand?: boolean;
   bgColor?: string;
   textColor?: string;
+  sizeRadius?: number;
+  actionButton?: boolean;
 }
-
+// quero que o padding seja diferente do botao de form, posso definir um padrao dentro de constants
+// e adc uma prop pra saber se é botao do form ou nao e assim mudar o padding
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-  padding: ${BUTTON_PADDING};
+  padding: ${({ actionButton }) => (actionButton ? `${ACTION_BUTTON_PADDING}` : `${BUTTON_PADDING}`)};
   width: ${({ expand }) => (expand ? '100%' : 'auto')};
   background-color: ${({ bgColor, disabled }) => (disabled ? '#d3d3d3' : bgColor || 'blueviolet')};
   color: ${({ textColor, disabled }) => (disabled ? '#f5f5f5' : textColor || 'white')};
   border: none;
-  border-radius: ${BUTTON_BORDER_RADIUS};
+  border-radius: ${({ sizeRadius }) => (sizeRadius ? `${sizeRadius}px` : `${BUTTON_BORDER_RADIUS}`)};
   text-align: center;
 `;
--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -8,11 +8,30 @@ interface ButtonProps {
   expand?: boolean;
   onClick?: (e: React.FormEvent) => Promise<string | null | undefined> | void;
   disable?: boolean;
+  sizeRadius?: number;
+  actionButton?: boolean;
 }
 
-export const Button = ({ title, onClick, bgColor, textColor, expand, disable }: ButtonProps) => {
+export const Button = ({
+  title,
+  onClick,
+  bgColor,
+  textColor,
+  expand,
+  disable,
+  sizeRadius,
+  actionButton,
+}: ButtonProps) => {
   return (
-    <ButtonContainer bgColor={bgColor} textColor={textColor} expand={expand} onClick={onClick} disabled={disable}>
+    <ButtonContainer
+      actionButton={actionButton}
+      bgColor={bgColor}
+      sizeRadius={sizeRadius}
+      textColor={textColor}
+      expand={expand}
+      onClick={onClick}
+      disabled={disable}
+    >
       {title}
     </ButtonContainer>
   );
