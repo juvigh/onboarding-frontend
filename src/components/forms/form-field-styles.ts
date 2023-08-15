27a8a19ea01346c8ef176571e3d7179e5d26d674
Julio <juliolk.4321@gmail.com>
2023-08-15 13:44:27 -0300

    Refactor FormField and add Input and InputSelect

--- src/components/forms/form-field-styles.ts
+++ src/components/forms/form-field-styles.ts
@@ -5,10 +5,16 @@ import { INPUT_BORDER_RADIUS, INPUT_PADDING } from '../../utils/constants';
 export const FormFieldContainer = styled.div`
   display: flex;
   flex-direction: column;
+`;
 
-  input {
-    padding: ${INPUT_PADDING};
-    border-radius: ${INPUT_BORDER_RADIUS};
-    border: ${INPUT_BORDER};
-  }
+export const Input = styled.input`
+  padding: ${INPUT_PADDING};
+  border-radius: ${INPUT_BORDER_RADIUS};
+  border: ${INPUT_BORDER};
+`;
+
+export const InputSelect = styled.select`
+  padding: ${INPUT_PADDING};
+  border-radius: ${INPUT_BORDER_RADIUS};
+  border: ${INPUT_BORDER};
 `;
--- src/components/forms/form-field.tsx
+++ src/components/forms/form-field.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Separator } from '../separator/separator';
 import { ErrorMessage } from './error-message';
-import { FormFieldContainer } from './form-field-styles';
+import { FormFieldContainer, Input, InputSelect } from './form-field-styles';
 
 interface FormFieldProps {
   label: string;
@@ -9,12 +9,18 @@ interface FormFieldProps {
   isButtonClicked: boolean;
   value: string;
   type?: string;
+  children?: React.ReactNode;
 }
 
-export const FormField = ({ label, onChange, isButtonClicked, value, type }: FormFieldProps) => {
+export const FormField = ({ label, onChange, isButtonClicked, value, type, children }: FormFieldProps) => {
   const emptyField = !value.trim() && isButtonClicked;
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    onChange(newValue);
+  };
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const newValue = e.target.value;
     onChange(newValue);
   };
@@ -23,7 +29,14 @@ export const FormField = ({ label, onChange, isButtonClicked, value, type }: For
     <FormFieldContainer>
       <label>{label}</label>
       <Separator vertical size={4} />
-      <input onChange={handleInputChange} value={value} type={type || 'text'} />
+      {type === 'select' ? (
+        <InputSelect onChange={handleSelectChange} value={value}>
+          {children}
+        </InputSelect>
+      ) : (
+        <Input onChange={handleInputChange} value={value} type={type || 'text'} />
+      )}
+
       <div className="caption-message">{emptyField && <ErrorMessage message="O campo é obrigatório" />}</div>
     </FormFieldContainer>
   );
