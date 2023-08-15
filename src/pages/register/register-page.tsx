1563e31ce72ce4d125a91bd3e6919faac00da46c
Julio <juliolk.4321@gmail.com>
2023-08-15 18:21:52 -0300

    Add token validation function to Register and Home page

--- src/pages/register/register-page.tsx
+++ src/pages/register/register-page.tsx
@@ -1,10 +1,21 @@
+import { useEffect } from 'react';
 import { RegisterForm } from '../../components/forms/register-form';
 import { Container } from './register-styles';
+import { validateToken } from '../../utils/validate-token';
+import { useNavigate } from 'react-router-dom';
 
 export const RegisterPage = () => {
+  const token = localStorage.getItem('authToken') ?? '';
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (validateToken(token)) {
+      navigate('/login');
+    }
+  }, [token]);
   return (
     <Container>
-      <RegisterForm />
+      <RegisterForm token={token} />
     </Container>
   );
 };
