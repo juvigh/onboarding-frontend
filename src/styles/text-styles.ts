864b746bd889baebc75d658a46e337c7300c8804
Julio <juliolk.4321@gmail.com>
2023-08-16 13:01:08 -0300

    Add "hasPadding" prop to Paragraph style component

--- src/styles/text-styles.ts
+++ src/styles/text-styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+const PADDING_PARAGRAPH = '16px 0';
+interface ParagraphProps {
+  hasPadding?: boolean;
+}
+
 export const H1 = styled.h1`
   text-align: center;
   margin: 0;
@@ -10,6 +15,7 @@ export const H3 = styled.h3`
   margin: 0;
 `;
 
-export const Paragraph = styled.p`
+export const Paragraph = styled.p<ParagraphProps>`
   margin: 0;
+  ${({ hasPadding }) => hasPadding && `padding:${PADDING_PARAGRAPH};`}
 `;
