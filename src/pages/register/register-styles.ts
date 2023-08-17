d4aa3501c83dfe90b66992b413706afa43a8566c
Julio <juliolk.4321@gmail.com>
2023-08-17 15:13:09 -0300

    Update register-styles.ts

--- src/pages/register/register-styles.ts
+++ src/pages/register/register-styles.ts
@@ -1,6 +1,7 @@
 import { styled } from 'styled-components';
 
 const MARGIN_SIDE = 400;
+const MARGIN_VERTICAL = '60px';
 export const Container = styled.div`
-  margin: 60px ${MARGIN_SIDE}px;
+  margin: ${MARGIN_VERTICAL} ${MARGIN_SIDE}px;
 `;
