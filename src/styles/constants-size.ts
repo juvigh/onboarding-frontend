fbfffd350bfac0c7773a7a0f04786f30c4485b95
Julio <juliolk.4321@gmail.com>
2023-08-18 15:57:26 -0300

    Separating typography into individual constants

---
+++ src/styles/constants-size.ts
@@ -0,0 +1,22 @@
+export const textSize = {
+  high: '24px',
+  medium: '16px',
+  small: '12px',
+};
+
+export const textWeight = {
+  bold: 700,
+  regular: 400,
+};
+
+export const separatorSize = {
+  large: 12,
+  medium: 8,
+  small: 4,
+};
+
+export const radius = {
+  radius: '8px',
+  radiusLarge: '12px',
+  xRadiusLarge: '20px',
+};
